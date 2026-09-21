f4e374e327e2a37ed4ccec6b845e8157eb991559
s01063040779-cmyk <s01063040779@gmail.com>
2026-09-21 17:27:58 +0900

    4주차 실습 소스코드

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <windows.h>
+
 void exerc1(void) {
 	double inch, cm;
 	scanf("%lf", &inch);
 	cm = inch * 2.54;
 	printf("%lf inch는 %lf cm입니다.\n", inch, cm);
 }
+
 void exerc2(void) {
 	double F, C;
 	scanf("%lf", &C);
-	F = (C * 1.8) +32;
+	F = (C * 1.8) + 32;
 	printf("섭씨 %lf도는 화씨 %lf도 입니다.\n", C, F);
 }
+
 void exerc3(void) {
 	double r, area;
 	const double PI = 3.141592;
@@ -21,6 +25,7 @@ void exerc3(void) {
 }
 
 int main(void) {
+	SetConsoleOutputCP(CP_UTF8);
 	exerc1();
 	exerc2();
 	exerc3();
