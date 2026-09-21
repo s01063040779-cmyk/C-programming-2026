c6327e9041a7e528aa3740b3e975d0b1c180e26e
s01063040779-cmyk <s01063040779@gmail.com>
2026-09-21 15:27:31 +0900

    4주차 과제 제출

--- main.c
+++ main.c
@@ -1,5 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int main() {
-	printf("Hello, World!\n");
+void exerc1(void) {
+	double inch, cm;
+	scanf("%lf", &inch);
+	cm = inch * 2.54;
+	printf("%lf inch는 %lf cm입니다.\n", inch, cm);
+}
+void exerc2(void) {
+	double F, C;
+	scanf("%lf", &C);
+	F = (C * 1.8) +32;
+	printf("섭씨 %lf도는 화씨 %lf도 입니다.\n", C, F);
+}
+void exerc3(void) {
+	double r, area;
+	const double PI = 3.141592;
+	scanf("%lf", &r);
+	area = PI * r * r;
+	printf("반지름 %lf인 원의 넓이는 %lf입니다.\n", r, area);
+}
+
+int main(void) {
+	exerc1();
+	exerc2();
+	exerc3();
 	return 0;
 }
